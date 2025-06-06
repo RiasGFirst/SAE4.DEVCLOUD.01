5260b3d0de0f601ddf6937d54dfee4c8c426e9b7
Sutaai <sutaai@proton.me>
2025-06-06 10:55:27 +0200

    feat: auto create compte courant on user create

--- backend/backend/routes/api/user.py
+++ backend/backend/routes/api/user.py
@@ -1,10 +1,11 @@
+from typing import Annotated
 import pydantic
 from fastapi import APIRouter, HTTPException, Response, status
 from tortoise.contrib.pydantic import pydantic_model_creator
 from tortoise.exceptions import IntegrityError
 
 from backend.auth import CurrentUser
-from backend.models import TypeUtilisateur, Utilisateur
+from backend.models import Compte, TypeCompte, TypeUtilisateur, Utilisateur
 
 router = APIRouter()
 
@@ -25,7 +26,12 @@ class CreateUserPayload(pydantic.BaseModel):
     role: TypeUtilisateur
 
 
-@router.post("/", response_model=pydantic_model_creator(Utilisateur))
+class CreateUserResponse(pydantic.BaseModel):
+    user: Annotated[Utilisateur, pydantic_model_creator(Utilisateur)]
+    account: Annotated[Compte, pydantic_model_creator(Compte)]
+
+
+@router.post("/", response_model=CreateUserResponse)
 async def create_user(payload: CreateUserPayload):
     """Crée un nouveeau utilisateur en fonction des paramètres donnés."""
     try:
@@ -35,7 +41,8 @@ async def create_user(payload: CreateUserPayload):
             password=payload.mot_de_passe,
             role=payload.role,
         )
-        return user
+        compte = await Compte.create(utilisateur=user, type_compte=TypeCompte.COURANT, solde=0, validated=True)
+        return {"user": user, "account": compte}
     except IntegrityError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="User already exists"
