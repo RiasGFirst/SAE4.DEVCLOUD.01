b9e9def76856fd193883bccffb53c6bd770757a7
RiasGFirst <gremoryrias69@proton.me>
2025-06-11 15:52:13 +0000

    feat: update Dockerfiles for backend and frontend, add healthcheck, and streamline start script

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,105 +1,75 @@
 #!/bin/bash
 
-# Chemin vers le fichier d'authentification
-AUTH_FILE="bauth.txt"
+AUTH_FILE="accounts.txt"
 
-# Codes de couleur
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
-# Fonction pour générer une chaîne aléatoire
 generate_random() {
     tr -dc 'a-zA-Z0-9' </dev/urandom | head -c "$1"
 }
 
-# Charger les variables du fichier .env s'il existe
-load_env() {
-    if [ -f ".env" ]; then
-        echo "${GREEN} ✔️ Chargement des variables d'environnement depuis .env ${NC}"
-        export $(grep -v '^#' .env | xargs)
-    else
-        echo -e "${RED}❌ Fichier .env introuvable.${NC}"
-        exit 1
-    fi
-}
+# load_env() {
+#     if [ -f ".env" ]; then
+#         echo "${GREEN}✔️ Chargement des variables d'environnement...${NC}"
+#         export $(grep -v '^#' .env | xargs)
+#     else
+#         echo -e "${RED}❌ Fichier .env introuvable.${NC}"
+#         exit 1
+#     fi
+# }
 
-# Activer l'environnement virtuel
-activate_venv() {
-    if [ -f "venv/bin/activate" ]; then
-        echo -e "${GREEN}✔️ Environnement virtuel trouvé. Activation...${NC}"
-        source venv/bin/activate
-    else
-        echo -e "${RED}❌ Environnement virtuel non trouvé. Création...${NC}"
-        python3 -m venv venv
-        source venv/bin/activate
-        pip install -r requirements.txt
-    fi
-}
-
-# Vérifier si l'API est accessible et répond "pong"
 check_api() {
     if [ -z "$API_HOST" ]; then
-        echo -e "${RED}❌API_HOST n'est pas défini dans le .env.${NC}"
+        echo -e "${RED}❌ API_HOST n'est pas défini.${NC}"
         exit 1
     fi
-
-    echo -e "${YELLOW}🔍Vérification de l'API à l'adresse $API_HOST...${NC}"
-
-    # Faire une requête GET à l'API et vérifier la réponse
+    echo -e "${YELLOW}🔍 Vérification API à $API_HOST...${NC}"
     response=$(curl -s "$API_HOST/api/ping")
     if [ "$response" == '"pong"' ]; then
-        echo -e "${GREEN}✔️ API est accessible.${NC}"
+        echo -e "${GREEN}✔️ API accessible.${NC}"
     else
-        echo -e "${RED}❌API n'est pas accessible. Veuillez vérifier l'URL.${NC}"
+        echo -e "${RED}❌ API inaccessible.${NC}"
         exit 1
     fi
 }
 
-
-# Lancer Django
 run_django() {
-    echo -e "${GREEN}🚀Démarrage du serveur Django...${NC}"
+    echo -e "${GREEN}🚀 Démarrage du serveur Django...${NC}"
+    
+    echo "USER: $(grep 'username:' "$AUTH_FILE" | cut -d ' ' -f2)"
+    echo "EMAIL: $(grep 'email:' "$AUTH_FILE" | cut -d ' ' -f2)"
+    echo "PASSWORD: $(grep 'mdp:' "$AUTH_FILE" | cut -d ' ' -f2)"
+
     python webclient/manage.py runserver 0.0.0.0:8000
 }
 
-
-# Charger les variables .env
-load_env
-
-# Vérifier si l'API est accessible
+# load_env
 check_api
 
-# Vérifie si le fichier bauth.txt existe
 if [ -f "$AUTH_FILE" ]; then
-    echo -e "${GREEN}✔️ Fichier $AUTH_FILE trouvé.${NC}"
-    activate_venv
+    echo -e "${GREEN}✔️ $AUTH_FILE trouvé.${NC}"
     run_django
 else
-    echo -e "${YELLOW}⚠️ Fichier $AUTH_FILE introuvable. Création de nouveaux identifiants...${NC}"
-
+    echo -e "${YELLOW}⚠️ $AUTH_FILE manquant. Création...${NC}"
     USERNAME="user_$(generate_random 6)"
     EMAIL="${USERNAME}@example.com"
     PASSWORD="$(generate_random 12)"
-
     echo "username: $USERNAME" > "$AUTH_FILE"
     echo "email: $EMAIL" >> "$AUTH_FILE"
     echo "mdp: $PASSWORD" >> "$AUTH_FILE"
 
-    # faire une requête POST pour enregistrer les identifiants
-    echo -e "${YELLOW}⚙️ Enregistrement des identifiants dans l'API...${NC}"
     curl -s -X POST "$API_HOST/api/user" \
-            -H "Content-Type: application/json" \
-            -d '{
-                "nom": "'"$USERNAME"'",
-                "email": "'"$EMAIL"'",
-                "mot_de_passe": "'"$PASSWORD"'",
-                "role": "agent_bancaire"
-            }'
+         -H "Content-Type: application/json" \
+         -d '{
+             "nom": "'"$USERNAME"'",
+             "email": "'"$EMAIL"'",
+             "mot_de_passe": "'"$PASSWORD"'",
+             "role": "agent_bancaire"
+         }'
 
-    echo -e "${GREEN}✔️ Identifiants enregistrés avec succès.${NC}"
-    echo -e "${YELLOW}⚙️ Activation de l'environnement virtuel et démarrage de Django...${NC}"
-    activate_venv
+    echo -e "${GREEN}✔️ Identifiants créés et envoyés.${NC}"
     run_django
 fi
